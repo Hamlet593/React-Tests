3df612610ec691aaab0d884ef45a4ac86710a623
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-09-05 18:43:44 +0400

    Adding alerts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import SaveButton from './helpers/ShowModal.js';
+import { useForm } from 'react-hook-form';
 
 function App() {
 
-  const [title, setTitle] = useState('');
-  const [trueTitle, setTrueTitle] = useState(true);
+  // This part is for limit logics
+
+  let [title, setTitle] = useState('');
+  let [trueTitle, setTrueTitle] = useState(true);
+
+  let [description, setDescription] = useState('');
+  let [trueDescription, setTrueDescription] = useState(true);
 
   useEffect(() => {
     if (title.length === 0 || title.length > 60) {
@@ -13,11 +18,56 @@ function App() {
     } else setTrueTitle(true);
   }, [title]);
 
+  useEffect(() => {
+    if (description.length > 170) {
+      setTrueDescription(false);
+    } else setTrueDescription(true);
+  }, [description]);
+
+  // This part is for alerts
+
+  let [showResults, setShowResults] = useState(false);
+
+  const AlertSuccess = () => (
+    <div className="alert alert-success p-2 w-full items-center justify-center flex top-0 z-75 bg-green-600 text-white" role="alert" style={{
+      color: 'white',
+      backgroundColor: "#27b86f"
+    }}>
+      SEO successfully saved.
+    </div>
+  )
+
+  const AlertError = () => (
+    <div className="alert alert-danger p-2 w-full items-center justify-center flex top-0 z-75 bg-green-600 text-white" role="alert" style={{
+      color: 'white',
+      backgroundColor: "#ed0b0b"
+    }}>
+      You are almost done. Please resolve errors for fields highlighted in red.
+    </div>
+  )
+
+  // This part is for useForm hook
+
+  const {
+    register,
+    formState: {
+      errors,
+    },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  }
 
   return (
     <div className="App">
       <div className="pt-7 pb-2 px-1">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {showResults && ((trueTitle && trueDescription) ? <AlertSuccess /> : <AlertError />)}
+
+          {/* SEO Title */}
+
           <div className="row mb-4">
             <div className="col-4">
               <h6 className="mb-1">SEO Title</h6>
@@ -26,14 +76,31 @@ function App() {
             <div className="col-8">
               <div className="form-field-container mb-3">
                 <div className="form-field position-relative ">
-                  <input id="og_title" name="og_title" type="text" autoComplete="off" required="" className=" active" value={title} onChange={evt => {
-                    setTitle(evt.target.value);
-                  }} />
+
+                  <input
+                    type='text'
+                    {...register('Seo Title', {
+                      required: `Can't be blank`,
+                      maxLength: {
+                        value: 60,
+                        message: `Is too long (maximum is 60 characters)`
+                      }
+                    })}
+                    onChange={evt => {
+                      setTitle(evt.target.value);
+                    }}
+                  />
+
+                  <div style={{ height: 20, color: 'red' }}>
+                    {errors?.firstName && <p>{errors?.firstName?.message}</p>}
+                  </div>
                 </div>
                 <small className="form-text text-muted text-right">Characters left: {60 - title.length}</small>
               </div>
             </div>
           </div>
+
+          {/* SEO Description */}
 
           <div className="row mb-4">
             <div className="col-4">
@@ -43,20 +110,45 @@ function App() {
             <div className="col-8">
               <div className="form-field-container ">
                 <div className="form-field position-relative ">
-                  <textarea id="og_description" name="og_description" aria-describedby="" autoComplete="off" className=" active" maxLength="170"></textarea>
+
+                  <input
+                    type='text'
+                    {...register('Seo Description', {
+                      maxLength: {
+                        value: 170,
+                        message: `Is too long (maximum is 170 characters)`
+                      }
+                    })}
+                    onChange={evt => {
+                      setDescription(evt.target.value);
+                    }}
+                  />
+
+                  <div style={{ height: 20, color: 'red' }}>
+                    {errors?.lastName && <p>{errors?.lastName?.message}</p>}
+                  </div>
+
                 </div>
-                <small className="form-text text-muted text-right">0 / 170</small>
+                <small className="form-text text-muted text-right">Characters left: {170 - description.length}</small>
               </div>
             </div>
           </div>
           <div className="pt-3 text-right">
-            <SaveButton
-              trueTitle={trueTitle}
+            <input
+              className="btn btn-sm btn-outline-primary mnw-100 py-1 text-uppercase weight-400 fz-12"
+              type='submit'
+              value='Ուղարկիր'
+              onClick={() => {
+                setShowResults(true);
+                setTimeout(() => {
+                  setShowResults(false)
+                }, 2000);
+              }}
             />
           </div>
         </form>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
